50983beaa2b2aae7620a9aa1c288cd77a5cc4230
zzx67 <zixuan940607@gmail.com>
2019-07-25 21:55:37 +1000

    add state code, json format

--- ssm-crud/src/main/java/com/zzx/crud/controller/EmployeeController.java
+++ ssm-crud/src/main/java/com/zzx/crud/controller/EmployeeController.java
@@ -7,10 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.zzx.crud.bean.Employee;
+import com.zzx.crud.bean.Msg;
 import com.zzx.crud.service.EmployeeService;
 
 /**
@@ -24,8 +26,19 @@ public class EmployeeController {
 	@Autowired
 	EmployeeService employeeService;
 	
-	//查询之前只需调用，穿入页码以及每页大小。
 	@RequestMapping("/emps")
+	@ResponseBody
+	public Msg getEmpsWithJson(@RequestParam(value="pn",defaultValue = "1")Integer pn) {
+		PageHelper.startPage(pn, 5);
+		List<Employee> emps = employeeService.getAll();
+		//使用pageinfo包装查询后的结果
+		//封装了详细的分页信息，包括有查询出来的数据,传入连续显示的页数
+		PageInfo page = new PageInfo(emps,5);
+		return Msg.success().add("pageInfo", page);
+	}
+	
+	//查询之前只需调用，穿入页码以及每页大小。
+	//@RequestMapping("/emps")
 	public String getEmps(@RequestParam(value="pn",defaultValue = "1")Integer pn,Model model) {
 		PageHelper.startPage(pn, 5);
 		List<Employee> emps = employeeService.getAll();
